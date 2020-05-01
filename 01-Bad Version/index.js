81277f944e760d24c709838b8c50f533fb020938
manjunath00 <kadrimanjunath56@gmail.com>
2020-05-01 22:51:18 +0530

    Solution of first bad version

---
+++ 01-Bad Version/index.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let recursive = function (start, end) {
+      if (start > end) return start;
+      let mid = Math.floor((start + end) / 2);
+      if (isBadVersion(mid)) {
+        return recursive(start, mid - 1);
+      } else {
+        return recursive(mid + 1, end);
+      }
+    };
+    return recursive(1, n);
+  };
+};
